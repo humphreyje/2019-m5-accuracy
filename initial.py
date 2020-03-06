dae433d005124e46abf7117c414f284a1d974865
humphreyje <humphreyje@gmail.com>
2020-03-05 22:19:51 -0800

    inital commit

---
+++ initial.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  5 22:17:31 2020
+
+@author: jonathan
+"""
+
+import pandas as pd
+
+filePath = '/home/jonathan/Documents/kaggle_comp/2019-m5-accuracy/'
+fileName = 'calendar.csv'
+calendar = pd.read_csv(filePath + fileName)
